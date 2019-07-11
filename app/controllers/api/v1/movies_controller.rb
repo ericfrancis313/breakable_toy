700ae5b02cba7ee6f6d942bec28f7ba2b02acd9a
Eric <EricFrancis313@gmail.com>
2019-07-10 21:37:00 -0400

    adding user

---
+++ app/controllers/api/v1/movies_controller.rb
@@ -0,0 +1,46 @@
+  require 'httparty'
+  require 'uri'
+  require 'net/http'
+  require 'date'
+
+  class Api::V1::MoviesController < ApplicationController
+    def show
+      @movies = Movies.all
+    end
+
+    def search
+
+      if params["date"]['restaurant']['coordinates']['latitude']
+        lat= params["date"]['restaurant']['coordinates']['latitude']
+      end
+
+      if params["date"]['restaurant']['coordinates']['longitude']
+        long= params["date"]['restaurant']['coordinates']['longitude']
+      end
+
+      date = Time.now.strftime("%Y/%m/%d")
+      zip = params["date"]["distance"]
+      time = params["date"]["time"]
+
+      url="https://api.amctheatres.com/v2/showtimes/views/current-location/#{lat}/#{long}/#{date}"
+
+      key =	ENV['MOVIE_KEY']
+
+      header = {
+      'Authorization'=> "Basic #{ENV['MOVIE_AUTH']}",
+      'client' => 'LAUN',
+      'x-api-key' =>"Bearer #{key}",
+      'territory'=> "US",
+      'api-version' => "v200",
+      'geolocation' => "#{lat};#{long}",
+      'X-AMC-Vendor-Key'=> '31dd5ae1-9562-4b1f-b718-f8b1a3a97492'
+      }
+
+      response =HTTParty.get(url, {
+        headers: header
+        })
+
+
+      render json: response
+    end
+  end
---
+++ app/javascript/react/components/Movies.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Movies = (props) => {
+
+  return (
+  <div className="movies">
+    <p className='movietitle'>Movies playing near you!</p>
+    <img className='moviepic'src='https://images-na.ssl-images-amazon.com/images/I/61S8ubjtbbL._SY500_.jpg'/>
+    <img className='moviepic' src='https://www.joblo.com/assets/images/joblo/posters/2019/04/D3LbtK7UEAAOMl1.jpg'/>
+    <img className='moviepic' src='https://amc-theatres-res.cloudinary.com/v1553857440/amc-cdn/production/2/movies/56500/56505/Poster/267883R1.jpg'/>
+    <img className='moviepic' src='https://cnet2.cbsistatic.com/img/x7wuV8zJXZqcLlS69HizMkSYYv0=/940x0/2019/04/02/0f0b1a02-e399-4c62-b409-c92a2b1904d2/reald3d.jpg'/>
+  </div>
+
+  );
+}
+
+export default Movies;
